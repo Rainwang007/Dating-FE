509cb289a22239f9012ecb76721cddec59b4b724
rainwang007 <rainwangjiantao@gmail.com>
2023-08-24 15:24:00 +0800

    basic frame

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+// src/store/index.js
+import { createStore } from 'redux';
+
+const initialState = {
+  // 初始状态
+};
+
+const reducer = (state = initialState, action) => {
+  // 处理动作
+  return state;
+};
+
+const store = createStore(reducer);
+
+export default store;
